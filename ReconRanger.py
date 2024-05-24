71334d0ef76b8cae4b75b4690c9cc3911912f2bc
Nouman404 <73934639+Nouman404@users.noreply.github.com>
2024-05-24 23:46:38 +0200

    Give stars pls :)

---
+++ ReconRanger.py
@@ -0,0 +1,20 @@
+from functions import get_current_user_and_group
+import os
+import sys
+
+user_name, group_name = get_current_user_and_group()
+
+target_script_path = "create_template.py"
+list_flags = ""
+for flag in sys.argv[1:]:
+    if "-sT" in flag or "-sU" in flag:
+        list_flags += flag.split("=")[0] + "=\"" + flag.split("=")[1] + "\""
+    else:
+        list_flags += flag + " "
+
+command = f'sudo python3 {target_script_path} -U {user_name}:{group_name} {list_flags}'
+exit_code = os.system(command)
+
+if exit_code != 0:
+    print(f"Script execution failed with exit code {exit_code}")
+    exit()
